c8ae05b3cb8129be288bf71a8b6902838dd4a19f
Bogwonch <bogwonch@bogwonch.net>
2015-12-21 11:22:20 +0000

    updates based on feedback

--- figures/benchmarks.R
+++ figures/benchmarks.R
@@ -5,7 +5,7 @@ setEPS()
 postscript("benchmarks.eps", width=6, height=4)
 
 data <- read.csv('benchmarks.csv', header=TRUE)
-qplot(Principals, Check, data=data, group=Delegations, color=Delegations, geom="line") +
+qplot(Principals, Check, data=data, group=Delegations, linetype=Delegations, geom="line") +
   scale_x_log10(breaks=c(10,100,500,1000)) +
   scale_y_log10(breaks=c(1,10,100)) +
   ylab("Check time (s)") +
